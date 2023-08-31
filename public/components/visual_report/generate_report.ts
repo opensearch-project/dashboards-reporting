13288ec3f3e5a441f50411fcdaf70562bd64d1a1
Miki <miki@amazon.com>
2023-08-31 04:22:34 -0700

    Make the generated report use the correct background color (#165)
    Also:
    * Remove font and color overrides from generated reports
    * Remove side-nav from the exported report
    * Avoid creating a footer when it has no content
    * Fix report dimensions
    
    Signed-off-by: Miki <miki@amazon.com>

--- public/components/visual_report/generate_report.ts
+++ public/components/visual_report/generate_report.ts
@@ -61,8 +61,10 @@ const removeNonReportElements = (
   doc
     .querySelectorAll("button[class^='euiButton']:not(.visLegend__button)")
     .forEach((e) => e.remove());
-  // remove top navBar
-  doc.querySelectorAll("[class^='euiHeader']").forEach((e) => e.remove());
+
+  // remove anything that shouldn't be shared
+  doc.querySelectorAll('.hide-for-sharing').forEach((e) => e.remove());
+
   // remove visualization editor
   if (reportSource === VISUAL_REPORT_TYPE.visualization) {
     doc.querySelector('[data-test-subj="splitPanelResizer"]')?.remove();
@@ -84,6 +86,8 @@ const addReportHeader = (doc: Document, header: string) => {
 };
 
 const addReportFooter = (doc: Document, footer: string) => {
+  // If there is no content, don't bother creating an element
+  if (!footer) return;
   const footerHtml = `<div id="reportingFooter">
     <div class="mde-preview" data-testid="mde-preview">
       <div class="mde-preview-content">${footer}</div>
@@ -102,19 +106,6 @@ const addReportStyle = (doc: Document, style: string) => {
   styleElement.innerHTML = style;
   doc.getElementsByTagName('head')[0].appendChild(styleElement);
   doc.body.style.paddingTop = '0px';
-};
-
-const computeHeight = (height: number, header: string, footer: string) => {
-  let computedHeight = height;
-  const headerLines = header.split('\n').length;
-  const footerLines = footer.split('\n').length;
-  if (headerLines) {
-    computedHeight += 24 * headerLines;
-  }
-  if (footerLines) {
-    computedHeight += 50 + 24 * footerLines;
-  }
-  return computedHeight;
 };
 
 export const generateReport = async (id: string, forceDelay = 15000) => {
@@ -179,12 +170,31 @@ export const generateReport = async (id: string, forceDelay = 15000) => {
     await timeout(1000);
   }
 
-  const width = document.documentElement.scrollWidth;
-  const height = computeHeight(
-    document.documentElement.scrollHeight,
-    header,
-    footer
-  );
+  /* The left-nav impacts the dimensions of the report; `#opensearch-dashboards-body`
+   * is the safest way to know the width.
+   * Since `html2canvas` without `foreignObjectRendering` copies computed styles of
+   * elements, we cannot rely on re-calculation of cloned widths after we remove all
+   * `.hide-for-sharing` elements in `removeNonReportElements`. The only option is to
+   * produce a narrower report when left-nav is docked.
+   *
+   * If `#opensearch-dashboards-body` is not found, it will fall back to `scrollWidth`.
+   */
+  const innerBodyDims = document.getElementById('opensearch-dashboards-body')?.getBoundingClientRect();
+  const width = innerBodyDims?.width || document.documentElement.scrollWidth;
+  const height = document.documentElement.scrollHeight;
+
+  const documentBackgroundColor: string = (window.getComputedStyle(document.documentElement) as CSSStyleDeclaration).backgroundColor;
+  const bgColor = documentBackgroundColor.startsWith('#')
+    ? documentBackgroundColor
+    // convert rgb() and rgba() to hex
+    : '#' + documentBackgroundColor.split(/[,()]/, 4).slice(1).map(v => parseInt(v).toString(16)).join('');
+
+  /* ToDo: `html2canvas` doesn't copy stylesheets when `foreignObjectRendering` is false. As a result
+   *       `@font-family` definitions are lost; find a way to get them from the document and add
+   *       them to the cloned document.
+   * ToDo: Don't add the header and footer to the document before cloning as they mess with what the
+   *       users see.
+   */
   return html2canvas(document.body, {
     scrollX: 0,
     scrollY: 0,
@@ -192,6 +202,7 @@ export const generateReport = async (id: string, forceDelay = 15000) => {
     windowHeight: height,
     width,
     height,
+    backgroundColor: bgColor,
     imageTimeout: 30000,
     useCORS: true,
     removeContainer: false,
@@ -199,6 +210,8 @@ export const generateReport = async (id: string, forceDelay = 15000) => {
     foreignObjectRendering: useForeignObjectRendering,
     onclone: function (documentClone) {
       removeNonReportElements(documentClone, reportSource);
+      // When the left nav is docked, the body element gets left-padded; this forces it to left-align
+      documentClone.body.style.padding = '0px';
       if (!useForeignObjectRendering) {
         addReportHeader(documentClone, header);
         addReportFooter(documentClone, footer);
@@ -259,6 +272,9 @@ export const generateReport = async (id: string, forceDelay = 15000) => {
       } else {
         const orient = canvas.width > canvas.height ? 'landscape' : 'portrait';
         const pdf = new jsPDF(orient, 'px', [canvas.width, canvas.height]);
+        pdf.setFillColor(bgColor);
+        // `+ 10` to fill in any uncolored areas that appear on the right and bottom edges of the PDF
+        pdf.rect(0, 0, canvas.width + 10, canvas.height + 10, "F");
         pdf.addImage(canvas, 'JPEG', 0, 0, canvas.width, canvas.height);
         pdf.save(fileName);
       }
