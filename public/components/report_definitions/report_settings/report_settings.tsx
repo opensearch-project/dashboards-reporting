757d2484aa99927ec2768203aed829f1a8afc4ac
Simeon Widdis <sawiddis@amazon.com>
2024-09-10 11:27:58 -0700

    Update large record report warning (#429)
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- public/components/report_definitions/report_settings/report_settings.tsx
+++ public/components/report_definitions/report_settings/report_settings.tsx
@@ -22,9 +22,7 @@ import {
   EuiCompressedCheckboxGroup,
   EuiCompressedComboBox,
   EuiFormRow,
-  OuiCallOut,
-  EuiText,
-  EuiIcon,
+  EuiCallOut,
 } from '@elastic/eui';
 import {
   REPORT_SOURCE_RADIOS,
@@ -801,22 +799,26 @@ export function ReportSettings(props: ReportSettingProps) {
           />
         </EuiCompressedFormRow>
         <EuiSpacer />
+        {savedSearchRecordLimit > 10000 ? (
+          <>
+            <EuiCallOut
+              color="primary"
+              title={i18n.translate(
+                'opensearch.reports.reportSettingProps.form.savedSearchLargeRecordLimitWarning',
+                { defaultMessage: 'Generating reports with a large number of records can cause memory issues' }
+              )}
+              iconType="iInCircle"
+              size="s"
+            />
+            <EuiSpacer size="xs" />
+          </>
+        ) : null}
         <EuiFormRow
           id="reportSourceSavedSearchRecordLimit"
           label={i18n.translate(
             'opensearch.reports.reportSettingProps.form.savedSearchRecordLimit',
             { defaultMessage: 'Record limit' }
           )}
-          helpText={
-            savedSearchRecordLimit > 10000 ? (
-              <EuiText color="warning" size="xs">
-                <EuiIcon color="warning" type="alert" size="s" /> Generating
-                very large reports can cause memory issues.
-              </EuiText>
-            ) : (
-              ''
-            )
-          }
         >
           <EuiFieldNumber
             value={savedSearchRecordLimit}
